a80aa74ab110178285dd796c22f9f5ab7bbe5a09
Jason Lin <jclin2.2009@gmail.com>
2023-12-16 10:49:22 -0800

    take screenshots + resize

--- capture.py
+++ capture.py
@@ -4,6 +4,23 @@ from PIL import Image
 import numpy as np
 import os
 
+def resize(frame):
+    '''
+    Takes an image, resizes it, and returns the resized opencv image
+    '''
+     # Convert the frame to a PIL image
+    pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+    # Resize the image
+    max_size = 250
+    ratio = max_size / max(pil_img.size)
+    new_size = tuple([int(x*ratio) for x in pil_img.size])
+    resized_img = pil_img.resize(new_size, Image.LANCZOS)
+
+    # Convert the PIL image back to an OpenCV image
+    frame = cv2.cvtColor(np.array(resized_img), cv2.COLOR_RGB2BGR)
+    return frame
+
 # Folder
 folder = "frames"
 
@@ -11,37 +28,24 @@ folder = "frames"
 frames_dir = os.path.join(os.getcwd(), folder)
 os.makedirs(frames_dir, exist_ok=True)
 
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
-
-# Check if the webcam is opened correctly
-if not cap.isOpened():
-    raise IOError("Cannot open webcam")
-
-# Wait for the camera to initialize and adjust light levels
-time.sleep(2)
-
 while True:
-    ret, frame = cap.read()
-    if ret:
-        # Convert the frame to a PIL image
-        pil_img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    # Capture screenshot
+    os.system(f"screencapture {folder}/screen.png")
 
-        # Resize the image
-        max_size = 250
-        ratio = max_size / max(pil_img.size)
-        new_size = tuple([int(x*ratio) for x in pil_img.size])
-        resized_img = pil_img.resize(new_size, Image.LANCZOS)
+    # Read the screenshot
+    frame = cv2.imread(f"{folder}/screen.png")
 
-        # Convert the PIL image back to an OpenCV image
-        frame = cv2.cvtColor(np.array(resized_img), cv2.COLOR_RGB2BGR)
+    # if frame == previous_frame:
+    #     print("Duplicate frame, skipping")
+    #     continue
+    
+    # frame = resize(frame)
 
-        # Save the frame as an image file
-        print("📸 Say cheese! Saving frame.")
-        path = f"{folder}/frame.jpg"
-        cv2.imwrite(path, frame)
-    else:
-        print("Failed to capture image")
+    # Save the frame as an image file
+    print("📸 Say cheese! Saving frame.")
+    path = f"{folder}/frame.jpg"
+    cv2.imwrite(path, frame)
+   
 
     # Wait for 2 seconds
     time.sleep(2)
@@ -49,3 +53,4 @@ while True:
 # Release the camera and close all windows
 cap.release()
 cv2.destroyAllWindows()
+
---
+++ sc.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("screencapture frames/screen.png")
